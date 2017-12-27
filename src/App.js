4d19647511c347aea5fe60c2a51350cf021f279d
Cezar <grigorecezar@yahoo.com>
2017-12-27 21:56:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './Raspberry_Pi_Logo.svg';
+import './App.css';
+
+class App extends Component {
+   constructor(){
+       super();
+       this.state ={temperatures: []};
+   }
+   componentDidMount() {
+          fetch('/get/temperature')
+            .then(res => {
+                console.log(res);
+                return res.json()
+             })
+            .then(temperatures => { 
+                console.log(temperatures); 
+                this.setState({ temperatures })
+             });
+         }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Silo Temperature Sensor</h1>
+        </header>
+        <p className="App-intro">
+            {this.state.temperatures.map(temperature =>
+              <div key={temperature.id}>The last read temperature is: <b>{temperature.temp}</b>°C.</div>
+            )} 
+        </p>
+      </div>
+    );
+  }
+
+}
+
+export default App;
