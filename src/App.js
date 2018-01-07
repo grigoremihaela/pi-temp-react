6cbf05744c6218303c82dcb6ddce1706a7268de1
Cezar <grigorecezar@yahoo.com>
2018-01-07 19:39:28 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('/get/temperature')
+    fetch('/demo/arsenal.json')
     .then(res => {
       return res.json()
     })
@@ -29,44 +29,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           {this.state.temperatures.map(temperature =>
-            <div key={temperature.id}>
-              <table className="App-table">
-  <tr>
-    <th>Pin Number</th>
-    <th>Sensor Number</th> 
-    <th>Temperature(°C)</th>
-  </tr>
-  <tr>
-    <td>4</td>
-    <td>1</td> 
-    <td><b>{temperature.temp41}</b></td>
-  </tr>
-  <tr>
-    <td>4</td>
-    <td>2</td> 
-    <td><b>{temperature.temp42}</b></td>
-  </tr>
-  <tr>
-    <td>4</td>
-    <td>3</td> 
-    <td><b>{temperature.temp43}</b></td>
-  </tr>
-  <tr>
-    <td>4</td>
-    <td>4</td> 
-    <td><b>{temperature.temp44}</b></td>
-  </tr>
-  <tr>
-    <td>17</td>
-    <td>1</td> 
-    <td><b>{temperature.temp17}</b></td>
-  </tr>
-  <tr>
-    <td>22</td>
-    <td>1</td> 
-    <td><b>{temperature.temp22}</b></td>
-  </tr>
-</table>
+            <div key={temperature.id}> {temperature.name}
+
             </div>
           )} 
         </p>
